ad86940f05833e282ebbabe0e8a26318ba4a3ead
SeijinWright <110748080+SeijinWright@users.noreply.github.com>
2022-08-13 12:38:02 -0700

    Enemy bump physics added

--- Assets/Scripts/GoodGuy.cs
+++ Assets/Scripts/GoodGuy.cs
@@ -8,7 +8,13 @@ public class GoodGuy : MonoBehaviour
     public float speed;
     private float horizontalInput;
     private float verticalInput;
-    
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     void FixedUpdate()
     {
         Move();
@@ -21,6 +27,6 @@ public class GoodGuy : MonoBehaviour
         Vector2 movement;
         movement.x = horizontalInput * speed * Time.deltaTime;
         movement.y = verticalInput * speed * Time.deltaTime;
-        transform.Translate(movement);
+        rb.velocity = movement;
     }
 }
